dc192ae58c11e48d00c9077d6756708ee20c6266
ryhab <ryhabelmoncerr@gmail.com>
2024-04-27 21:29:05 +0100

    first commit

---
+++ src/main/java/com/Keycloak_RESTAPI/Keycloak_RESTAPI/KeycloakRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.Keycloak_RESTAPI.Keycloak_RESTAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakRestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Keycloak_RESTAPI/Keycloak_RESTAPI/KeycloakRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Keycloak_RESTAPI.Keycloak_RESTAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
